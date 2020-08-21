0b2fbee76b82215033d8770de853cd3477ee871f
Fujiharuu <54049669+Fujiharuu@users.noreply.github.com>
2020-08-21 22:30:32 +0800

    In Case I Destory this 80% completed code

--- Engine/Source/GridControllerSystem.cpp
+++ Engine/Source/GridControllerSystem.cpp
@@ -11,12 +11,12 @@ void GridControllerSystem::CreateGrids()
 		}
 	}
 	int y = 0;
-	for (int Posy = 500; Posy > -500; Posy -= 50)
+	for (int Posy = -500; Posy < 500; Posy += 50)
 	{
 		int x = 0;
-		for (int Posx = -500; Posx < 500; Posx += 50)
+		for (int Posx = 500; Posx > -500; Posx -= 50)
 		{
-			GridPosition[x][y] = glm::vec3(Posx, 2, Posy);
+			GridPosition[x][y] = glm::vec3(Posy, 2, Posx);
 			CheckGridCost(x,y);
 			//std::cout << GridPosition[count].x << ", " << GridPosition[count].y << ", " << GridPosition[count].z << " ::::" << GridCost[count] << std::endl;
 			x++;
@@ -66,9 +66,9 @@ void GridControllerSystem::GetDestinationGrid()
 {
 	// Check Which Grid the destination is at
 	glm::vec2 destination = glm::vec2(-1,-1);	// If it is -1 at the end, means that destination is impossible
-	for (int y = 0; y < 20; ++y)
+	for (int x = 0; x < 20; ++x)
 	{
-		for (int x = 0; x < 20; ++x)
+		for (int y = 0; y < 20; ++y)
 		{
 			glm::vec3 GridTopLeft = GridPosition[x][y];
 			glm::vec3 GridBottomRight = glm::vec3(GridTopLeft.x + 50, GridTopLeft.y, GridTopLeft.z - 50);
@@ -97,62 +97,83 @@ void GridControllerSystem::CreatePath(glm::vec2 Destination)
 {
 	int dX = Destination.x;
 	int dY = Destination.y;
-	glm::vec3 GridTopLeft = GridPosition[dX][dY];
-	glm::vec3 GridBottomRight = glm::vec3(GridTopLeft.x + 50, GridTopLeft.y, GridTopLeft.z - 50);
+	//glm::vec3 GridTopLeft = GridPosition[dX][dY];
+	glm::vec3 GridBottomRight = glm::vec3(GridPosition[dX][dY].x + 50, GridPosition[dX][dY].y, GridPosition[dX][dY].z - 50);
+	bool north, south, east, west;
+	north = south = east = west = false;
 	// Check at the edge of the world space
 	// If not, Check if the grid around it is passable or not
-	// World Position is from -500,500 to 500,-500
-	for (int y = 0; y < 20; ++y)
-	{
-		for (int x = 0; x < 20; ++x)
-		{
+	
 			//North
-			if (GridTopLeft.z != 500)
-			{
-				// There is a Grid in the North Direction!!
-				// Check the North Grid ID
+	if (GridPosition[dX][dY].z != 500)
+	{
+		// There is a Grid in the North Direction!!
+		// Check the North Grid ID
 
-				if ((GridPosition[x][y].z == (GridTopLeft.z + 50)) && (GridPosition[x][y].x == GridTopLeft.x) && (GridCost[x][y] == -1)) // Checks if there is grid Cost is uninitialized
-				{
-					GridCost[x][y] = GridCost[dX][dY] + 1;
-				}
-
-			}
-			// South
-			if (GridBottomRight.z != -500)
-			{
-				// There is a Grid in the South Direction!!
-				// Check the South Grid ID
-
-				if ((GridPosition[x][y].z == GridTopLeft.z - 50) && (GridPosition[x][y].x == GridTopLeft.x) && (GridCost[x][y] == -1)) // Checks if there is grid Cost is uninitialized
-				{
-					GridCost[x][y] = GridCost[dX][dY] + 1;
-				}
-			}
-			// East
-			if (GridBottomRight.x != 500)
-			{
-				// There is a Grid in the East Direction!
-				// Check East Grid ID
-
-				if ((GridPosition[x][y].x == GridBottomRight.x + 50) && (GridPosition[x][y].z == GridTopLeft.z) && (GridCost[x][y] == -1))// Checks if there is grid Cost is uninitialized
-				{
-					GridCost[x][y] = GridCost[dX][dY] + 1;
-				}
-
-			}
-			// West
-			if (GridTopLeft.x != -500)
-			{
-				// There is a Grid in the West Direction!
-				// Check West Grid ID
-				if ((GridPosition[x][y].x == GridTopLeft.x - 50) && (GridPosition[x][y].z == GridTopLeft.z) && (GridCost[x][y]) == -1) // Check if there is grid Cost is uninitialized
-				{
-					GridCost[x][y] = GridCost[dX][dY] + 1;
-				}
-
-			}
+		if ((GridPosition[dX-1][dY].z == (GridPosition[dX][dY].z + 50)) &&
+			(GridPosition[dX-1][dY].x == GridPosition[dX][dY].x) && (GridCost[dX-1][dY] == -1)) // Checks if there is grid Cost is uninitialized
+		{
+			GridCost[dX-1][dY] = GridCost[dX][dY] + 1;
+			north = true;
 		}
+
+	}
+	// South
+	if (GridBottomRight.z != -500)
+	{
+		// There is a Grid in the South Direction!!
+		// Check the South Grid ID
+
+		if ((GridPosition[dX + 1][dY].z == GridPosition[dX][dY].z - 50) && (GridPosition[dX + 1][dY].x == GridPosition[dX][dY].x) &&
+			(GridCost[dX + 1][dY] == -1)) // Checks if there is grid Cost is uninitialized
+		{
+			GridCost[dX + 1][dY] = GridCost[dX][dY] + 1;
+			south = true;
+		}
+	}
+	// East
+	if (GridBottomRight.x != 500)
+	{
+		// There is a Grid in the East Direction!
+		// Check East Grid ID
+
+		if ((GridPosition[dX][dY + 1].z == GridBottomRight.z + 50) && (GridPosition[dX][dY + 1].x == GridBottomRight.x) &&
+			(GridCost[dX][dY + 1] == -1))// Checks if there is grid Cost is uninitialized
+		{
+			GridCost[dX][dY + 1] = GridCost[dX][dY] + 1;
+			east = true;
+		}
+
+	}
+	// West
+	if (GridPosition[dX][dY].x != -500)
+	{
+		// There is a Grid in the West Direction!
+		// Check West Grid ID
+		if ((GridPosition[dX][dY - 1].x == GridPosition[dX][dY].x - 50) && (GridPosition[dX][dY - 1].z == GridPosition[dX][dY].z) &&
+			(GridCost[dX][dY - 1]) == -1) // Check if there is grid Cost is uninitialized
+		{
+			GridCost[dX][dY - 1] = GridCost[dX][dY] + 1;
+			west = true;
+		}
+
+
+	}
+	if (north == true)
+	{
+		CreatePath(glm::vec2(dX-1,dY));
+	}
+	if (south == true)
+	{
+		CreatePath(glm::vec2(dX+1,dY));
+	}
+	if (east == true)
+	{
+		CreatePath(glm::vec2(dX,dY+1));
+	}
+	if (west == true)
+	{
+		CreatePath(glm::vec2(dX,dY-1));
 	}
 }
 
