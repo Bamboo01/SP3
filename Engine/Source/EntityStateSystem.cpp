36f303a87f0f839c362ffe602b9a94b2cc16ba3d
Bamboo01 <48957444+Bamboo01@users.noreply.github.com>
2020-08-22 16:09:54 +0800

    FIXED ENTITY STATE SYSTEM

--- Engine/Source/EntityStateSystem.cpp
+++ Engine/Source/EntityStateSystem.cpp
@@ -2,9 +2,9 @@
 
 void EntityStateSystem::Setup()
 {
-	Signature signature;
-	signature.set(coordinator.GetComponentType<EntityState>());
-	coordinator.SetSystemSignature<EntityStateSystem>(signature);
+    Signature signature;
+    signature.set(coordinator.GetComponentType<EntityState>());
+    coordinator.SetSystemSignature<EntityStateSystem>(signature);
 }
 
 void EntityStateSystem::Init()
@@ -14,46 +14,43 @@ void EntityStateSystem::Init()
 
 void EntityStateSystem::Update(float dt)
 {
-	std::vector<Entity> ActivateList;
-	std::vector<Entity> DeactivateList;
+    std::vector<Entity> ActivateList;
+    std::vector<Entity> DeactivateList;
 
-	std::vector <std::vector<Entity>::iterator> inactivelistlist;
+    //Check all deactivated entities and see if they need to be activated
+    for (auto it = InactiveEntities.begin(); it != InactiveEntities.end();)
+    {
+        auto const& entitystate = coordinator.GetComponent<EntityState>(*it);
+        if (entitystate.active)
+        {
+            ActivateList.push_back(*it);
+            it = InactiveEntities.erase(it);
+        }
+        else
+        {
+            ++it;
+        }
+    }
 
-	//Check all deactivated entities and see if they need to be activated
-	for (auto it = InactiveEntities.begin(); it != InactiveEntities.end(); ++it)
-	{
-		auto const& entitystate = coordinator.GetComponent<EntityState>(*it);
-		if (entitystate.active)
-		{
-			ActivateList.push_back(*it);
-			inactivelistlist.push_back(it);
-		}
-	}
+    //Checks if any entities that are not deactivated by the coordinator need to be deactivated
+    for (auto const& entity : m_Entities)
+    {
+        auto& entitystate = coordinator.GetComponent<EntityState>(entity);
+        if (!entitystate.active)
+        {
+            DeactivateList.push_back(entity);
+            InactiveEntities.push_back(entity);
+        }
+    }
 
-	for (auto it : inactivelistlist)
-	{
-		InactiveEntities.erase(it);
-	}
+    //Activation and Deactivation
+    for (auto entity : ActivateList)
+    {
+        coordinator.ActivateEntity(entity);
+    }
 
-	//Checks if any entities that are not deactivated by the coordinator need to be deactivated
-	for (auto const& entity : m_Entities)
-	{
-		auto& entitystate = coordinator.GetComponent<EntityState>(entity);
-		if (!entitystate.active)
-		{
-			DeactivateList.push_back(entity);
-			InactiveEntities.push_back(entity);
-		}
-	}
-
-	//Activation and Deactivation
-	for (auto entity : ActivateList)
-	{
-		coordinator.ActivateEntity(entity);
-	}
-
-	for (auto entity : DeactivateList)
-	{
-		coordinator.DeactivateEntity(entity);
-	}
+    for (auto entity : DeactivateList)
+    {
+        coordinator.DeactivateEntity(entity);
+    }
 }
